e7ef041bfe6b42f30f5bd06e420b98008fb09855
Mindy D Tran <mindytran98@gmail.com>
2024-01-22 15:08:07 -0500

    create employeelsit component

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,16 @@
+import EmployeeListItem from "./EmployeeListItem";
+
+function EmployeeList(){
+    return(
+        <div>
+            <EmployeeListItem/>
+            <EmployeeListItem/>
+            <EmployeeListItem/>
+            <EmployeeListItem/>
+            <EmployeeListItem/>
+            <EmployeeListItem/>
+        </div>
+    )
+}
+
+export default EmployeeList;
