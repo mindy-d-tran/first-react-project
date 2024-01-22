e3328f64bf07049d3f86ba937dc060e3d57c8768
Mindy D Tran <mindytran98@gmail.com>
2024-01-22 14:47:45 -0500

    add search bar

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import SearchBar from "./SearchBar";
 
 function HomePage() {
   return (
     <div>
       <Header></Header>
+      <SearchBar></SearchBar>
     </div>
   );
 }
