dfdd05ad11d67ca8f3f70ab2cb824917d2b29896
Mindy D Tran <mindytran98@gmail.com>
2024-01-22 16:08:15 -0500

    create employeepage component

---
+++ src/components/EmployeePage.js
@@ -0,0 +1,11 @@
+import Header from "./Header";
+
+function EmployeePage(){
+    return (
+        <div id="employeePage">
+            <Header></Header>
+        </div>
+    )
+}
+
+export default EmployeePage;
