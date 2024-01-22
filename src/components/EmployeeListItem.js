96189958c1ebd791ae76597ea47e8ed3a982018d
Mindy D Tran <mindytran98@gmail.com>
2024-01-22 15:08:37 -0500

    create employeelistitem component

---
+++ src/components/EmployeeListItem.js
@@ -0,0 +1,12 @@
+function EmployeeListItem (){
+    return (
+        <div>
+            <img src="#"/>
+            <div>
+                <h2>Name</h2>
+                <p>Role</p>
+            </div>
+        </div>
+    )
+}
+export default EmployeeListItem;
