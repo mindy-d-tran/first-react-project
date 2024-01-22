cf638859a5a3d44b87eb6338239561c344a56022
Mindy D Tran <mindytran98@gmail.com>
2024-01-22 14:47:18 -0500

    create search bar component

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+function SearchBar(){
+    return (
+        <div>
+            <form>
+                <input type="text"></input>
+            </form>
+        </div>
+    )
+}
+export default SearchBar;
