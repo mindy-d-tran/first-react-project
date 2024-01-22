2dd78d20a69ae2c513fe29ad0834a41ba38dedd3
Mindy D Tran <mindytran98@gmail.com>
2024-01-22 15:43:25 -0500

    change class to classname and add id

--- src/components/EmployeeListItem.js
+++ src/components/EmployeeListItem.js
@@ -1,9 +1,9 @@
 function EmployeeListItem (){
     return (
-        <div>
-            <img src="#"/>
-            <div>
-                <h2>Name</h2>
+        <div id="employeeListItem">
+            <img src="https://i.pinimg.com/564x/1a/c0/23/1ac0231aa6d74d092328d445aa98183e.jpg" alt="profile image"/>
+            <div className="nameRole">
+                <h3>Name</h3>
                 <p>Role</p>
             </div>
         </div>
